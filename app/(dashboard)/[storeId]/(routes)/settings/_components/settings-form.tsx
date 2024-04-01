77e518b795395f0181d288d593c093dfd7121cca
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-04-01 21:23:29 +0200

    Minor bug fix

--- app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
@@ -25,6 +25,7 @@ import { Separator } from "@/components/ui/separator";
 import { Heading } from "@/components/custom/components";
 import { AlertModal } from "@/components/modals/components";
 import ApiAlert from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -38,6 +39,7 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 const SettingsForm = ({ initialData }: SettingsFormProps) => {
     const params = useParams();
     const router = useRouter();
+    const origin = useOrigin();
 
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
