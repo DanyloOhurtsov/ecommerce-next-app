69ce34b178beff9ee0ee1610db07318b6530651c
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-04-08 15:52:55 +0200

    UPD Colors | Minor style and func changes

--- app/(dashboard)/[storeId]/(routes)/colors/_components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/_components/columns.tsx
@@ -2,6 +2,8 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import CellAction from "./cell-actions";
+import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
 
 export type ColorColumn = {
     id: string;
@@ -10,22 +12,36 @@ export type ColorColumn = {
     createdAt: string;
 };
 
+const onCopy = (value: string) => {
+    navigator.clipboard.writeText(value);
+    toast.success('Color copied')
+};
 export const columns: ColumnDef<ColorColumn>[] = [
     {
         accessorKey: "name",
         header: "Name",
+        cell: ({ row }) => (
+            <div className="flex gap-x-2 items-center">
+                <div
+                    className="w-6 h-6 rounded-full border"
+                    style={{ backgroundColor: row.original.value }}
+                />
+                <p>{row.original.name}</p>
+            </div>
+        ),
     },
     {
         accessorKey: "value",
         header: "Value",
         cell: ({ row }) => (
-            <div className="flex items-center gap-x-2">
-                <p className=" min-w-20">{row.original.value}</p>
-                <div
-                    className="w-6 h-6 rounded-full border"
-                    style={{ backgroundColor: row.original.value }}
-                />
-            </div>
+            <Button
+                onClick={() => onCopy(row.original.value)}
+                variant={"ghost"}
+                className="px-1.5 h-6 min-w-24 w-min flex items-center rounded-md text-left cursor-pointer"
+                style={{ border: `2px solid ${row.original.value}` }}
+            >
+                <p>{row.original.value.toUpperCase()}</p>
+            </Button>
         ),
     },
     {
