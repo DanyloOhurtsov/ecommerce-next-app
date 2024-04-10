ec5473a46f1617a16e0b87e0ca4282392df6c052
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-04-10 09:31:02 +0200

    feat(navbar, footer): Create Navbar and footer

--- ecommerce-client/app/layout.tsx
+++ ecommerce-client/app/layout.tsx
@@ -1,22 +1,28 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 
 const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Store",
-  description: "Store",
+    title: "Store",
+    description: "Store",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <Navbar />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
 }
